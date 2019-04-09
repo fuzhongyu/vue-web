12f02e600c953e7d774f9b19dcd325fcca4b0b31
fuzhongyu <fuzhongyu2@163.com>
2019-04-09 14:59:14 +0800

    Update permissionConfig.js

--- src/global/permissionConfig.js
+++ src/global/permissionConfig.js
@@ -9,23 +9,7 @@ const permissionConfig = {
   permissionDel: 'system:permission:del',
   roleView: 'system:role:view',
   roleEdit: 'system:role:edit',
-  roleDel: 'system:role:del',
-  // 元数据相关
-  homeView: 'metadata:home:view',
-  homeEdit: 'metadata:home:edit',
-  databaseView: 'metadata:database:view',
-  databaseEdit: 'metadata:database:edit',
-  databaseDel: 'metadata:database:del',
-  tableView: 'metadata:table:view',
-  tableEdit: 'metadata:table:edit',
-  tableDel: 'metadata:table:del',
-  columnView: 'metadata:column:view',
-  columnEdit: 'metadata:column:edit',
-  columnDel: 'metadata:column:del',
-  usecaseView: 'metadata:usecase:view',
-  usecaseEdit: 'metadata:usecase:edit',
-  usecaseDel: 'metadata:usecase:del',
-  sqlView: 'metadata:sql:view'
+  roleDel: 'system:role:del'
 }
 
 export default permissionConfig
